84628baf20a2aec2e30ec83d351371ea675b8343
fireonex <mari.krulikovskaya@mail.ru>
2024-05-22 21:47:54 +0300

    9th commit (component hw9 completed)

--- src/s2-homeworks/hw09/HW9.tsx
+++ src/s2-homeworks/hw09/HW9.tsx
@@ -3,10 +3,10 @@ import Clock from './Clock'
 import s2 from '../../s1-main/App.module.css'
 
 /*
-* 1 - в файле Clock.tsx дописать функции stop, start, onMouseEnter, onMouseLeave
-* 2 - в файле Clock.tsx из переменной date вычислить значения stringTime, stringDate, stringDay, stringMonth
-* 3 - в файле Clock.tsx дизэйблить кнопки стоп / старт если таймер не запущен / запущен соответственно
-* 4 - сделать стили в соответствии с дизайном
+* 1 - в файле Clock.tsx дописать функции stop, start, onMouseEnter, onMouseLeave (made)
+* 2 - в файле Clock.tsx из переменной date вычислить значения stringTime, stringDate, stringDay, stringMonth (made)
+* 3 - в файле Clock.tsx дизэйблить кнопки стоп / старт если таймер не запущен / запущен соответственно (made)
+* 4 - сделать стили в соответствии с дизайном (made)
 * */
 
 const HW9 = () => {
