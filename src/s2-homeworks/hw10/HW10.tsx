6545f1aebea360c01f032cd2d1e34ee011f68cdb
fireonex <mari.krulikovskaya@mail.ru>
2024-05-27 16:47:25 +0300

    10th commit (component hw10 completed)

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -7,18 +7,25 @@ import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
 
 /*
-* 1 - в файле loadingReducer.ts дописать типы и логику
-* 2 - получить isLoading из редакса
-* 3 - дописать функцию setLoading
+* 1 - в файле loadingReducer.ts дописать типы и логику (made)
+* 2 - получить isLoading из редакса (made)
+* 3 - дописать функцию setLoading  (made)
 * 4 - сделать стили в соответствии с дизайном
 * */
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    let isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
+
+    let dispatch = useDispatch()
+    //const isLoading = false
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
+        dispatch(loadingAC(true))
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 1500)
 
         // setTimeout
     }
